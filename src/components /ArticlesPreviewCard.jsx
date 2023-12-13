5c6c4b6a7352e6bf8a90d6b811662e66143be5de
MJahidH <m.jahid.h@hotmail.com>
2023-12-13 10:50:40 +0000

    link to created for each article to the main article

--- src/components /ArticlesPreviewCard.jsx
+++ src/components /ArticlesPreviewCard.jsx
@@ -1,10 +1,20 @@
+import MainArticleCard from "./MainArticleCard";
+import { Link } from "react-router-dom"
+
 const ArticlesPreviewCard = ({ article }) => {
   return (
     <div className="articles_preview_card">
       <h1>{article.title}</h1>
-      <img src={article.article_img_url} />
+      <img className="article_preview_card" src={article.article_img_url} />
       <p>Votes {article.votes}</p>
+      <h3>
+        <Link to={`/articles/${article.article_id}`} element={MainArticleCard}>
+        Go To Article
+        </Link>
+        
+        </h3>
       <p>{article.created_at}</p>
+      
     </div>
   );
 };
