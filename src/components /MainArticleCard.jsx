9c87955632b5b509797aa2efe5474a2bc0756552
MJahidH <m.jahid.h@hotmail.com>
2023-12-14 14:11:21 +0000

    error handling added

--- src/components /MainArticleCard.jsx
+++ src/components /MainArticleCard.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 const MainArticleCard = () => {
   const [article, setArticle] = useState([]);
   const { article_id } = useParams();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     getArticleById(article_id).then((article) => {
@@ -13,24 +14,37 @@ const MainArticleCard = () => {
   }, []);
 
   const handleClick = (event) => {
-    setArticle((currArticle) => ({
-      ...currArticle,
-      votes: currArticle.votes + 1,
-    }));
-    patchVotes({inc_votes : 1},article_id)
+    patchVotes({ inc_votes: 1 }, article_id)
+      .then(() => {
+        setArticle((currArticle) => ({
+          ...currArticle,
+          votes: currArticle.votes + 1,
+        }));
+      })
+      .catch((err) => {
+        setError({ err });
+      });
   };
 
-  return (
-    <div className="main_article_card">
-      <h1>{article.title}</h1>
-      <h2>{article.author}</h2>
-      <img className="main_card_image" src={article.article_img_url} />
-      <p>Votes {article.body}</p>
-      <p>Votes {article.votes}</p>
-      <button onClick={handleClick}>Upvote This Article</button>
-      <p>{article.created_at}</p>
-    </div>
-  );
+  if (error) {
+    return (
+      <>
+        <p>Comment Faild To Add</p>
+      </>
+    );
+  } else {
+    return (
+      <div className="main_article_card">
+        <h1>{article.title}</h1>
+        <h2>{article.author}</h2>
+        <img className="main_card_image" src={article.article_img_url} />
+        <p>Votes {article.body}</p>
+        <p>Votes {article.votes}</p>
+        <button onClick={handleClick}>Upvote This Article</button>
+        <p>{article.created_at}</p>
+      </div>
+    );
+  }
 };
 
 export default MainArticleCard;
